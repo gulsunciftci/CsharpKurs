fb417aaee4eae366c0473441baf6bbd70ba782cc
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-02-19 00:45:31 +0300

    collection konusuna dictionarylere çalışarak devam edildi

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                           //COLLECTİONS
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("book","kitap");
+            dictionary.Add("computer", "tablo");
+
+            Console.WriteLine(dictionary["book"]);
+            //Console.WriteLine(dictionary["glasses"]); //hata
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
+            Console.WriteLine(dictionary.Count);
+            Console.WriteLine(dictionary.ContainsKey("glass"));
+            Console.WriteLine(dictionary.ContainsKey("book"));
+        }
+    }
+}
