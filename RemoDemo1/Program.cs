22361db026e5decc8a8ebed3b5190968664874d6
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-02-18 16:13:36 +0300

    interfaceler ile temel bir implementasyon yapıldı

---
+++ RemoDemo1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new FileLogger();
+            customerManager.Add();
+            customerManager.Logger = new DatabaseLogger();
+            customerManager.Add();
+
+        }
+    }
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; } //property
+        public void Add()
+        {
+       
+            Logger.Log();
+            Console.WriteLine("Customer added");
+                
+        }
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database");
+
+        }
+    }
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+}
