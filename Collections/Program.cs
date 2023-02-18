8c40168ed6242a7f2cb47b73d216d3de27a1ac41
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-02-18 22:29:04 +0300

    collectionlar çalışıldı

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Array
+            //string[] cities = new string[] { "Ankara", "İstanbul" }; x
+            //cities[2] = "Adana"; //kızar
+                                 
+                               //COLLECTİONLAR
+            //ArrayList
+            ArrayList cities = new ArrayList();
+            cities.Add("Ankara");
+            cities.Add("Adana");
+            cities.Add("İstanbul");
+            cities.Add('T');
+            cities.Add(1);
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+        }
+    }
+}
---
+++ ReferenceAndValueType/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number1 = 10;
+            int number2 = 20;
+
+            //değer tipi
+            number2 = number1;
+            number1 = 30;
+            Console.WriteLine(number2);
+
+            //referans tipi
+            string[] cities = {"Ankara","Adana" }; //101
+            string[] cities1 = { "Bursa", "Bolu" }; //101
+            cities1 = cities; //cities in referansı citiec 1 e eşit olur
+            cities[0] = "istanbul";
+            Console.WriteLine(cities1[0]);//istanbul yazar
+
+            ////performans sorunu oluşturur
+            //DataTable dataTable = new DataTable();
+            //DataTable dataTable1 = new DataTable();
+            //dataTable1 = dataTable;
+
+            DataTable dataTable;
+            DataTable dataTable1 = new DataTable();
+            dataTable = dataTable1;
+
+
+
+        }
+    }
+}
