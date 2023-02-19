fece293b3ac6cbe3389d1bf6ddb789f7c4814282
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-02-19 21:56:17 +0300

    Ado Net projesi ve nesneleri oluşturuldu

---
+++ AdoNetDemo/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class Product //tablonun class karşılığı tekildir
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockAmount { get; set; }
+    }
+}
