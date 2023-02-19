b06664c1e1273c44b7acbd8fe23a24fd4c33e06b
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-02-20 00:11:40 +0300

    listeler ile veri listeleme çalışıldı

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -12,7 +12,7 @@ namespace AdoNetDemo
     public class ProductDal //veri çekme,gönderme gibi işlemler bulunur
     {
 
-        public DataTable GetAll()
+        public List<Product> GetAll()
         {
             //bağlantı nesnesi oluşturalım
             SqlConnection connection = new SqlConnection(@"server=(localdb)\ProjectModels;initial catalog=ETrade;integrated security=true");
@@ -31,8 +31,42 @@ namespace AdoNetDemo
 
             SqlDataReader reader = command.ExecuteReader();
 
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount= Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice= Convert.ToDecimal(reader[" UnitPrice"])
+
+                };
+                products.Add(product);
+                   
+            }
+        }
+        public DataTable GetAll2()
+        {
+            //bağlantı nesnesi oluşturalım
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\ProjectModels;initial catalog=ETrade;integrated security=true");
+            //@=tamamen string olarak kabul et demek
+            //initialcatalog=hangi veri tabanına bağlanacağını söyler
+            //integrated security=false yaparsak uzaktaki veri tabanına bağlanırız
+
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+
+            }
+            //"select * from Products" sorgusunu connection a göndeririz
+            SqlCommand command = new SqlCommand("select * from Products", connection); //kodu çalıştırmak için SqlCommand sınıfından yararlanırız
+
+
+            SqlDataReader reader = command.ExecuteReader();
+
             //Datatable oluşturup içini readerla doldurduk
-            DataTable dataTable = new DataTable();
+            DataTable dataTable = new DataTable(); //kullanılmaz çünkü pahalı bir nesnedir
             dataTable.Load(reader);
             reader.Close();
             connection.Close();
@@ -40,6 +74,5 @@ namespace AdoNetDemo
 
 
         }
-
     }
 }
